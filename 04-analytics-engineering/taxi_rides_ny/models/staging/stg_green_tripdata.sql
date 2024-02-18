5a6d6ba763b2d69645912943cc2ec4be0644ad55
Timothy Claytor <tim@claytor.me>
2024-02-18 23:13:40 +0000

    fixing some misspellings

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -1,42 +1,39 @@
-with 
+{{
+    config(
+        materialized='view'
+    )
+}}
 
-source as (
-
-    select * from {{ source('staging', 'green_tripdata') }}
-
-),
-
-renamed as (
-
-    select
-        vendor_id,
-        pickup_datetime,
-        dropoff_datetime,
-        store_and_fwd_flag,
-        rate_code,
-        passenger_count,
-        trip_distance,
-        fare_amount,
-        extra,
-        mta_tax,
-        tip_amount,
-        tolls_amount,
-        ehail_fee,
-        airport_fee,
-        total_amount,
-        payment_type,
-        {{ get_payment_type_description('payment_type') }} as payment_type_description,
-        distance_between_service,
-        time_between_service,
-        trip_type,
-        imp_surcharge,
-        pickup_location_id,
-        dropoff_location_id,
-        data_file_year,
-        data_file_month
-
-    from source
-
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendor_id, pickup_datetime) as rn
+  from {{ source('staging','green_tripdata') }}
+  where vendor_id is not null 
 )
+select
+    -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['vendor_id', 'pickup_datetime']) }} as trip_id,
+    {{ dbt.safe_cast("vendor_id", api.Column.translate_type("integer")) }} as vendor_id,
+    {{ dbt.safe_cast("rate_code", api.Column.translate_type("integer")) }} as ratecode_id,
+    {{ dbt.safe_cast("pickup_location_id", api.Column.translate_type("integer")) }} as pickup_location_id,
+    {{ dbt.safe_cast("dropoff_location_id", api.Column.translate_type("integer")) }} as dropoff_location_id,
+    
+    -- trip info
+    store_and_fwd_flag,
+    {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count,
+    cast(trip_distance as numeric) as trip_distance,
+    {{ dbt.safe_cast("trip_type", api.Column.translate_type("integer")) }} as trip_type,
 
-select * from renamed
+    -- payment info
+    {{ get_payment_type_description("payment_type") }} as payment_type_description
+from tripdata
+where rn = 1
+
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
